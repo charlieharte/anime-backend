40c953ace9163dfb0f0d52ffe36cd0cb755dde06
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-07-13 18:16:52 -0400

    adding a commit to show controllers, models, and routes folders created.

--- models/User.js
+++ models/User.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
+const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
-  name:{ 
+  name: { 
     type: String,
     required: [true, "can't be blank"],
-    match:
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: Email,
+    required: true
+  },
+  profile: {
+    firstName: String,
+    lastName: String,
+    bio: String
+  },
+  active: {
+    type: Boolean,
+    default: true
   }
 })
+
 mongoose.model('User', UserSchema);
